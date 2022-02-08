bf6c92b56b5c0b4b76b8ad8c0d7863df527cdc3e
scottmil <10510570+scottmil@users.noreply.github.com>
2022-02-08 12:36:45 -0800

    Fixed version number

--- drivers/EufyHomeBaseCamera.groovy
+++ drivers/EufyHomeBaseCamera.groovy
@@ -1,5 +1,5 @@
 /*  Eufy HomeBase Camera
- *  Version 1.1.1
+ *  Version 1.1.3
  *
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
